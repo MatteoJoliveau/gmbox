149241958461a18b7187a414011f55cbddd7aee3
Matteo Joliveau <matteojoliveau@gmail.com>
2022-11-02 11:02:20 +0100

    Bootstrap project
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        libraries = with pkgs;[
+          webkitgtk
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib.out
+          dbus.lib
+          openssl_3.out
+        ];
+
+        packages = with pkgs; [
+          curl
+          wget
+          pkg-config
+          dbus
+          openssl_3
+          glib
+          gtk3
+          libsoup
+          webkitgtk
+          nodePackages.pnpm
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook =
+            let
+              joinLibs = libs: builtins.concatStringsSep ":" (builtins.map (x: "${x}/lib") libs);
+              libs = joinLibs libraries;
+            in
+            ''
+              export LD_LIBRARY_PATH=${libs}:$LD_LIBRARY_PATH
+            '';
+        };
+      });
+}
